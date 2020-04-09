7b8edd231c8f53045b837771a91e592a668eba22
Jackson McQuality <mcquajac000@users.campus>
2020-04-09 15:04:01 -0500

    Setting Up Testing

---
+++ WpfApp/KASBUApp - Testing/UnitTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace KASBUApp___Testing
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
