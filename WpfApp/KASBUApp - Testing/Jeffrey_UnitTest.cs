a6692000ffa8f5a24220475d5686bbe95cd74dc8
Jeffrey Martin <jpmartin288@gmail.com>
2020-04-09 21:10:27 -0500

    Push Test

---
+++ WpfApp/KASBUApp - Testing/Jeffrey_UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace KASBUApp___Testing
+{
+    [TestFixture]
+    class Jeffrey_UnitTest
+    {
+        [SetUp]
+        public static void SetUp()
+        {
+
+        }
+
+        [TearDown]
+        public static void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
