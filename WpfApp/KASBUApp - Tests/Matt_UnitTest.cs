b747f9b5fc15cdec692032d3eac1dca5e9c753b8
Matthew White <mnw19971@hotmail.com>
2020-04-17 04:52:50 -0500

    Added Testing Class : No Functionality Yet

---
+++ WpfApp/KASBUApp - Tests/Matt_UnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NUnit.Framework;
+using WpfApp;
+
+
+namespace KASBUApp___Tests
+{
+    class Matt_UnitTest
+    {
+       
+        [SetUp]
+        public void setUp()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void Test_Input()
+        {
+
+        }
+    }
+}
