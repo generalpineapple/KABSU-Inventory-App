057daa8747abca4d2dbda06002dba86e4ed0dc60
bhartman24 <bhartman24@E1116W17>
2021-02-18 17:11:07 -0600

    created more tests for Search window

--- WpfApp/KASBUApp - Tests/SearchWindowTests.cs
+++ WpfApp/KASBUApp - Tests/SearchWindowTests.cs
@@ -78,31 +78,93 @@ namespace KASBUApp___Tests
         [Test]
         public void FilterOnAnimalName()
         {
-
+            searchWindow = new SearchWindow();
+            SearchResults searchResults = new SearchResults();
+            List<SearchResult> sr = new List<SearchResult>();
+            SearchTerm st = new SearchTerm("*", "*", "1 Oak", "*", "*", "*", "*");
+            sr = searchResults.retrieveData(st);
+            string AnimalName = "1 Oak";
+            List<SearchResult> results = searchWindow.CalculateResultList();
+            foreach(SearchResult s in results)
+            {
+                if (s.AnimalName.Equals(AnimalName))
+                {
+                    Assert.True(s.AnimalName.Equals(AnimalName));
+                }
+            }
         }
 
         [Test]
         public void FilterOnOwner()
         {
-
+            searchWindow = new SearchWindow();
+            SearchResults searchResults = new SearchResults();
+            List<SearchResult> sr = new List<SearchResult>();
+            SearchTerm st = new SearchTerm("*", "*", "*", "*", "KSU ASI", "*", "*");
+            sr = searchResults.retrieveData(st);
+            string owner = "KSU ASI";
+            List<SearchResult> results = searchWindow.CalculateResultList();
+            foreach (SearchResult s in results)
+            {
+                if (s.Owner.Equals(st.Owner))
+                {
+                    Assert.True(s.Owner.Equals(owner));
+                }
+            }
         }
 
         [Test]
         public void FilterOnBreed()
         {
-
+            searchWindow = new SearchWindow();
+            SearchResults searchResults = new SearchResults();
+            List<SearchResult> sr = new List<SearchResult>();
+            SearchTerm st = new SearchTerm("*", "*", "*", "Maine Anjou", "*", "*", "*");
+            sr = searchResults.retrieveData(st);
+            string breed = "Maine Anjou";
+            List<SearchResult> results = searchWindow.CalculateResultList();
+            foreach (SearchResult s in results)
+            {
+                if (s.Breed.Equals(st.Breed))
+                {
+                    Assert.True(s.Breed.Equals(breed));
+                }
+            }
         }
 
         [Test]
-        public void FilterOnTown()
+        public void FilterOnMispelledTownName()
         {
-
+            searchWindow = new SearchWindow();
+            SearchResults searchResults = new SearchResults();
+            List<SearchResult> sr = new List<SearchResult>();
+            SearchTerm st = new SearchTerm("*", "*", "*", "*", "*", "Manhattan", "*");
+            sr = searchResults.retrieveData(st);
+            string town = "Manhatten";
+            List<SearchResult> results = searchWindow.CalculateResultList();
+            foreach (SearchResult s in results)
+            {
+                if (s.Town.Equals(st.Town))
+                {
+                    Assert.True(!s.Town.Equals(town));
+                }
+            }
         }
 
         [Test]
-        public void FilterOnState()
+        public void FilterOnStateNotInDatabase()
         {
-
+            searchWindow = new SearchWindow();
+            SearchResults searchResults = new SearchResults();
+            List<SearchResult> sr = new List<SearchResult>();
+            SearchTerm st = new SearchTerm("*", "*", "*", "*", "*", "*", "CO");
+            sr = searchResults.retrieveData(st);
+            //string town = "Manhatten";
+            List<SearchResult> results = searchWindow.CalculateResultList();
+            foreach (SearchResult s in results)
+            {
+                Assert.True(!s.State.Equals(st.State));
+            }
         }
     }
 }
